4cf49638e833d41d51f1f83e0634357afdca6151
Oryan Hachamoff <oryanhach95@gmail.com>
2026-04-14 18:18:14 +0300

    added basic counter logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 import './App.css'
+import { useState } from 'react'
 
 export default function App() {
+	// states
+	const [text, setText] = useState('')
+
+	// variables
+	const wordCount = text.trim() === '' ? 0 : text.trim().split(/\s+/).length
+
 	return (
-		<main>
-			<h1>Character Counter</h1>
+		<main className="character-counter">
+			<h1 className="character-counter__title">Character Counter</h1>
+			<textarea
+				value={text}
+				onChange={(e) => setText(e.target.value)}
+				className="character-counter__textarea"></textarea>
+			<div className="character-counter__stats">
+				<div className="character-counter__stat">
+					<p className="character-counter__stat-label">Characters</p>
+					<p>{text.length}</p>
+				</div>
+				<div className="character-counter__stat">
+					<p className="character-counter__stat-label">Words</p>
+					<p>{wordCount}</p>
+				</div>
+			</div>
 		</main>
 	)
 }
